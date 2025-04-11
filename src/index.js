4bf4c93a854266fa4816e5e3d94d6e13c7ae8491
Master-harsh <harshtiwari7479@gmail.com>
2025-04-11 16:03:16 +0530

    dotenv files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello from backend course with chai.")
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(7000, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
