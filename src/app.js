4e126ea927f80e2c1663b3a5f5951dd3c41ccab1
Master-harsh <harshtiwari7479@gmail.com>
2025-04-15 16:12:49 +0530

    Add health check route and controller; fix asyncHandler export and ApiResponse constructor

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '16kb'}));
 app.use(express.static('public'));
 
+// import routes
+import healthCheckRoutes from './routes/healthCheck.routes.js';
+
+// routes
+
+app.use("/api/v1/healthcheck", healthCheckRoutes);
+
+
 export default app;
--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -5,4 +5,4 @@ const asyncHandler = (reqestaHandler) => {
     }
 }
 
-export default asyncHandler;
+export  { asyncHandler };
